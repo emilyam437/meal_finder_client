1e6b58e5ce1303e499e7b3638daea492c2a52038
emilyam437 <emilyam437@gmail.com>
2023-01-10 17:00:26 +0100

    second commit

--- index.js
+++ index.js
@@ -77,4 +77,4 @@ app.get("/", async (req, res) => {
 res.send('hello backend')
 });
 
-app.listen(8000, () => console.log('running on port 8000'))
+app.listen(process.env.PORT || 8000, () => console.log('running on port 8000'))
